570a53ae039f6a231c99bc99a5405b528b9ff21b
Divyansh Raj <rajdivyansh2000@gmail.com>
2023-09-02 02:33:22 +0530

    Updates

---
+++ precache-manifest.1b404df0ee689b00b896aa0329a799c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "da2e5c5c3138207beb92",
+    "url": "/TextUtils/static/css/main.2bd5beab.chunk.css"
+  },
+  {
+    "revision": "da2e5c5c3138207beb92",
+    "url": "/TextUtils/static/js/main.da2e5c5c.chunk.js"
+  },
+  {
+    "revision": "8c8ed3e6ce86a909c145",
+    "url": "/TextUtils/static/js/1.8c8ed3e6.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "df05ce19991abad81e6f",
+    "url": "/TextUtils/static/js/runtime~main.df05ce19.js"
+  },
+  {
+    "revision": "44d534e91f89ab62ba8240c57ee04021",
+    "url": "/TextUtils/index.html"
+  }
+];
