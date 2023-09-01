6ace7c247afc3d726a90c5c031f59598ccd13b05
Divyansh Raj <rajdivyansh2000@gmail.com>
2023-09-02 00:56:10 +0530

    remove react router dom for publishing to github pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import About from "./components/About";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import Alert from "./components/Alert";
 import React, { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
